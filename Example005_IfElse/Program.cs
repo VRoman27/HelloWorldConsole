53e37bb187a17bb78d4c40157b9589d243b0a7aa
VRoman27 <romavolodin1@rambler.ru>
2022-11-27 18:51:40 +0300

    add if else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя:");
+string username = Console.ReadLine();
+
+if(username.ToLower == "денис")
+{
+    Console.WriteLine("Денчик ушёл в металл!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
