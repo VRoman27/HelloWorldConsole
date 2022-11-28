f02b62cc87de3ac0e0ddb7e546010313aafc4bea
VRoman27 <romavolodin1@rambler.ru>
2022-11-28 20:52:39 +0300

    add FriendsAndDog

---
+++ Example008_ FriensAndDog/Program.cs
@@ -0,0 +1,22 @@
+﻿double speed_1 = 2, speed_2 = 3, speed_d = 5;
+double distance = 10000;
+double time = 0;
+bool it_is_friend_1 = false;
+int count = 0;
+
+while(distance>10)
+{
+    if(it_is_friend_1)
+    {
+        time = distance/(speed_1 + speed_d);
+        it_is_friend_1 =false;
+    }
+    else
+    {
+        time = distance/(speed_2 + speed_d);
+        it_is_friend_1 =true;
+    }
+    distance = distance - (speed_1 + speed_2) * time;
+    count++;
+}
+Console.WriteLine(count);
