63a5811eedb4180babbe3e3295fba0021eb893d4
VRoman27 <romavolodin1@rambler.ru>
2022-11-30 22:46:44 +0300

    add Lek_2

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int Max(int[] array)
+{
+    int result = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > result)
+        {
+            result = array[i];
+        }
+    }
+    return result;
+}
+
+int[] array = {51,22,43,64,75,16,79,88,29,100};
+
+Console.WriteLine(Max(array));
---
+++ Example010_ArrayLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexOf(int[] col, int find)
+{
+    int count = col.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (col[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = indexOf(array, 3);
+Console.WriteLine(pos);
